fb31d074ce1967511fc4b93b1a2d465f614961cb
Fastus001 <tkarczmarzyk@gmail.com>
2021-01-06 21:44:18 +0100

    first commit

---
+++ src/test/java/fastus/springframework/springboot/Mb2gSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package fastus.springframework.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mb2gSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
